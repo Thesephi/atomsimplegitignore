cc82e76266ca7e5a768380940622156992c55041
Khang Dinh <thesephi@gmail.com>
2018-09-09 13:15:04 +0200

    Initially scaffolded

---
+++ spec/atomsimplegitignore-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomsimplegitignoreView from '../lib/atomsimplegitignore-view';
+
+describe('AtomsimplegitignoreView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
